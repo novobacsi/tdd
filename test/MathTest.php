b2ace26c2cd2b1c63ff680581660ba35796ff3f3
Bela Novotny <bela.novotny@doclerholding.com>
2015-03-11 18:05:07 +0100

    init

---
+++ test/MathTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class description.
+ *
+ * @package
+ * @subpackage
+ */
+
+namespace Tdd\Test;
+
+use Tdd\Math;
+
+
+class MathTest extends \PHPUnit_Framework_TestCase
+{
+	public function testSum()
+	{
+		$this->assertEquals(2, Math::sum(1, 1));
+	}
+
+}
+

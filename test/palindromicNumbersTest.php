a6e7743c212745373aff4382c545b7230ad26f39
Bela Novotny <bela.novotny@doclerholding.com>
2015-06-11 08:31:27 +0200

    isPalindromicNumber failing Test

--- test/palindromicNumbersTest.php
+++ test/palindromicNumbersTest.php
@@ -6,6 +6,12 @@ use Tdd\PalindromicNumbers;
 
 class PalindromicNumbersTest extends \PHPUnit_Framework_TestCase
 {
+	public function testIsPalindromicNumber()
+	{
+		$palindromicNumber = new PalindromicNumbers();
+
+		$this->assertTrue($palindromicNumber->isPalindromic(11));
+	}
 
 }
 
